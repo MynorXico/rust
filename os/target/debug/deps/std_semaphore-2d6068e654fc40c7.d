e379cdf48b540ec826c9fd3ec8ec270f9faf5aad
Mynor Xico <xic14659@uvg.edu.gt>
2019-03-28 03:42:21 -0600

    finished

---
+++ os/target/debug/deps/std_semaphore-2d6068e654fc40c7.d
@@ -0,0 +1,5 @@
+/home/mynor/os/target/debug/deps/libstd_semaphore-2d6068e654fc40c7.rlib: /home/mynor/.cargo/registry/src/github.com-1ecc6299db9ec823/std-semaphore-0.1.0/src/lib.rs
+
+/home/mynor/os/target/debug/deps/std_semaphore-2d6068e654fc40c7.d: /home/mynor/.cargo/registry/src/github.com-1ecc6299db9ec823/std-semaphore-0.1.0/src/lib.rs
+
+/home/mynor/.cargo/registry/src/github.com-1ecc6299db9ec823/std-semaphore-0.1.0/src/lib.rs:
---
+++ os/target/debug/deps/test-64507a6287e00aab.d
@@ -0,0 +1,5 @@
+/home/mynor/os/target/debug/deps/test-64507a6287e00aab: src/main.rs
+
+/home/mynor/os/target/debug/deps/test-64507a6287e00aab.d: src/main.rs
+
+src/main.rs:
